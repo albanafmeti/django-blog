0ffe2c57d8964e4539e3d2b422ce4f6090ffe523
Alban Afmeti <alban.afmeti@softmogul.com>
2019-12-18 11:40:09 +0100

    added blog functionalities

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    path('', views.index, name="index"),
+    path('posts/', views.get_all, name="all-blog"),
+    path('posts/<int:post_id>/', views.show, name="single-post"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('contact/send', views.contact_send, name="contact-send"),
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.http import JsonResponse
+from django.core.mail import send_mail
 
 
 def index(request):
-    data = {"hello": "world"}
-    return render(request, "blog/index.html", context=data)
+    posts = Post.objects.all().order_by('-id')[:4]
+    return render(request, "blog/index.html", context={"posts": posts})
+
+
+def get_all(request):
+    posts = Post.objects.all().order_by('-id')
+    return render(request, "blog/all.html", context={"posts": posts})
+
+
+def show(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, "blog/show.html", context={"post": post})
+
+
+def about(request):
+    return render(request, "blog/about.html")
+
+
+def contact(request):
+    return render(request, "blog/contact.html")
+
+
+def contact_send(request):
+    name = request.POST['name'][0]
+    email = request.POST['email'][0]
+    phone = request.POST['phone'][0]
+    message = request.POST['message'][0]
+
+    send_mail(
+        'New contact message',
+        message,
+        name + " <" + email + ">",
+        ['albanafmeti@gmail.com'],
+        fail_silently=True,
+    )
+
+    return JsonResponse({
+        "success": True
+    })
